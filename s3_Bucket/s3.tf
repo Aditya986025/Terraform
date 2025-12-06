436a2bf28c6c9627da74127ed7c4ca9abd69a9ae
aditya <aditya@gmail.com>
2025-12-06 13:26:55 +0530

    commit s3

--- s3_Bucket/s3.tf
+++ s3_Bucket/s3.tf
@@ -11,3 +11,28 @@ tags = {
 
 }
 }
+
+## Block Public Access
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.bucket1.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+## acl
+resource "aws_s3_bucket_acl" "bucket1_acl" {
+    bucket = aws_s3_bucket.bucket1.id
+    acl    = "public-read"
+}
+
+## Versioning
+resource "aws_s3_bucket_versioning" "my_version" {
+  bucket = aws_s3_bucket.bucket1.id 
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
