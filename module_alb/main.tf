b6c3b397190ae2211aa30703bd1bb4259ba7daa9
aditya <aditya@gmail.com>
2025-12-09 21:49:36 +0530

    moudel_alb

--- module_alb/main.tf
+++ module_alb/main.tf
@@ -1,4 +1,7 @@
-module "alb" { 
-    source = "./abl.tf"
+module "alb" {
+  source = "./alb"
   
+  name  = "my-alb2"
+  security_group = ["sg-08d64a6a4e1a252bd"]
+  subnets = ["subnet-0bd2a6db82585a589"]
 }
