f824eff48f1a6604d6c6f2d49877424f1774bb4a
aditya <aditya@gmail.com>
2025-12-09 22:47:16 +0530

    Module_alb_project

--- module_alb/main.tf
+++ module_alb/main.tf
@@ -3,6 +3,9 @@ module "alb" {
   
   name  = "my-alb2"
   security_group = ["sg-08d64a6a4e1a252bd"]
-  subnets  = ["subnet-0bd2a6db82585a589"]
+  subnets  = [
+    "subnet-0bd2a6db82585a589", 
+    "subnet-0e40e13af310eb0f0"
+    ]
   vpc_id = "vpc-08d274b7fc12560a9"
 }
